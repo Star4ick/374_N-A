542ab2252789b47896b79986dd593803e6dd88ae
Star4ick <AnsonAlexandr2@mail.ru>
2020-11-05 18:13:54 +0300

    added lab06

---
+++ lab06/lab06.sh
@@ -0,0 +1,2 @@
+rm -f lab06.db
+cat lab06.sql | sqlite3 lab06.db
---
+++ lab06/lab06.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS game;
+CREATE TABLE game (account TEXT, money INT, avatar TEXT);
+
+INSERT INTO game VALUES ('Star4ick'		, 1000		, '374_N-A/lab06/img-apple.png'	);
